beec27c49da727c936c2ed98a6135784be0bc272
TerrySu <2684759855@qq.com>
2019-06-27 18:53:46 +0800

    simplify publish progress

--- scripts/publishToSite.ts
+++ scripts/publishToSite.ts
@@ -1,8 +1,8 @@
 import fs from 'fs-extra'
 import path from 'path'
 
-publish()
-export default function publish() {
+publishToSite()
+export default function publishToSite() {
   const publicPath = path.resolve( __dirname, "../public" )
   const sitePath = path.resolve( __dirname, "../Terry-Su.github.io" )
   fs.copySync( publicPath, sitePath )
